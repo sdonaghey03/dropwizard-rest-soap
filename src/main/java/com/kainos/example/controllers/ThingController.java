bad0c26e648449c4779c370131b6dc51d06d52a0
stephendo <s.donaghey@kainos.com>
2017-05-24 17:10:37 +0100

    Finished adding Mustache to template

--- src/main/java/com/kainos/example/controllers/ThingController.java
+++ src/main/java/com/kainos/example/controllers/ThingController.java
@@ -1,6 +1,5 @@
 package com.kainos.example.controllers;
 
-import com.kainos.example.api.ThingObject;
 import com.kainos.example.models.ThingModel;
 import com.kainos.example.views.ThingView;
 
@@ -21,14 +20,5 @@ public class ThingController {
     @Produces({MediaType.TEXT_HTML})
     public ThingView getThing(@PathParam("id") int id) {
         return new ThingView(thingModel.getThing(id));
-    }
-
-    @POST
-    @Path("/add")
-    @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_JSON})
-    public String addThing(ThingObject thingObject) {
-        thingModel.addThing(thingObject.getValue());
-        return ":)";
     }
 }
